16d66380e0923acd9c5baab5abd93a4671901a56
guilhemtrd <guilhem.trd@gmail.com>
2024-10-29 18:19:28 +0100

    🎨 Add dynamic time display with elapsed time format

--- src/components/discussion/Discussion.jsx
+++ src/components/discussion/Discussion.jsx
@@ -180,54 +180,42 @@ const Discussion = () => {
     function formatElapsedTime(dateString) {
         const date = new Date(dateString);
         const now = new Date();
-        console.log("now : " + now);
-        console.log("date : " + date);
 
         const diffInSec = Math.floor((now - date) / 1000);
-        console.log("diffInSec : " + diffInSec);
 
         const timezoneOffset = (new Date()).getTimezoneOffset();
         const timezoneOffsetInSec = timezoneOffset * 60;
-
+        
         const diffInSeconds = diffInSec - timezoneOffsetInSec;
-
         if (diffInSeconds < 0) {
             return 'à venir';
         }
-
         if (diffInSeconds < 15) {
             return 'à l\'instant';
         }
-
         if (diffInSeconds < 60) {
             return `il y a ${diffInSeconds} seconde${diffInSeconds > 1 ? 's' : ''}`;
         }
-
         const diffInMinutes = Math.floor(diffInSeconds / 60);
         if (diffInMinutes < 60) {
             return `il y a ${diffInMinutes} minute${diffInMinutes > 1 ? 's' : ''}`;
         }
-
         const diffInHours = Math.floor(diffInMinutes / 60);
         if (diffInHours < 24) {
             return `il y a ${diffInHours} heure${diffInHours > 1 ? 's' : ''}`;
         }
-
         const diffInDays = Math.floor(diffInHours / 24);
         if (diffInDays < 7) {
             return `il y a ${diffInDays} jour${diffInDays > 1 ? 's' : ''}`;
         }
-
         const diffInWeeks = Math.floor(diffInDays / 7);
         if (diffInWeeks < 4) {
             return `il y a ${diffInWeeks} semaine${diffInWeeks > 1 ? 's' : ''}`;
         }
-
         const diffInMonths = Math.floor(diffInDays / 30);
         if (diffInMonths < 12) {
             return `il y a ${diffInMonths} mois`;
         }
-
         const diffInYears = Math.floor(diffInDays / 365);
         return `il y a ${diffInYears} an${diffInYears > 1 ? 's' : ''}`;
     }
